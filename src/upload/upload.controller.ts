c1bf5e735549cf643fa9577c3b42059c6cf6940a
phong  nguyen <phongit1995@gmail.com>
2020-08-20 17:49:58 +0700

    end upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Post ,UseInterceptors,UploadedFile} from '@nestjs/common';
-import {ApiTags} from '@nestjs/swagger';
+import { Controller, Post ,UseInterceptors,UploadedFile, Get ,Response, Param} from '@nestjs/common';
+import {ApiTags ,ApiResponse} from '@nestjs/swagger';
 import {FileInterceptor}from '@nestjs/platform-express';
 import { multerOptions } from 'src/config/multer.config';
-@ApiTags("Upload")
+import {exists,createReadStream} from 'fs';
+import * as path from 'path';
+import { exit } from 'process';
+@ApiTags("Upload File")
 @Controller('upload')
 export class UploadController {
     @Post()
+    @ApiResponse({ status: 201, description: 'List User'})
     @UseInterceptors(FileInterceptor('file',multerOptions))
     uploadFile(@UploadedFile() file){
         console.log(file);
         return file ;
+    }
+    @Get(":name")
+    async getFile(@Response() res,@Param('name')name:string){
+        exists(path.join(__dirname,'./../../upload',name),(file)=>{
+            if(file){
+                createReadStream(path.join(__dirname,'./../../upload',name)).pipe(res);
+            }
+        })
     }
 }
